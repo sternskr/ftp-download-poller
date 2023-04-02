5403b0f27f54caf5576997f605a8c49c099defed
Kyle Sterns <sterns.mobile.krs@gmail.com>
2023-04-02 18:53:16 -0500

    Updated build file

--- build.sh
+++ build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # Build the Docker image with the specified tag
-docker build -t my-poll-ftp-image .
-
-# Log in to the Docker registry
-docker login --username=yourusername --password=yourpassword
+docker build -t ftp-download-poller .
 
 # Push the Docker image to the registry
-docker tag my-poll-ftp-image yourregistry/my-poll-ftp-image:latest
-docker push yourregistry/my-poll-ftp-image:latest
-
-# Log out of the Docker registry
-docker logout
+docker tag ftp-download-poller sternskr/ftp-download-poller:latest
+docker push sternskr/ftp-download-poller:latest
 
 # Remove the local Docker image
-docker rmi my-poll-ftp-image
+docker rmi ftp-download-poller
